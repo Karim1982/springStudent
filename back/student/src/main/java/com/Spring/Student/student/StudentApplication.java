9082e524c28ba9c978e38d5dc915594a15d58a9e
kimo <kimo84@gmail.com>
2022-05-28 12:38:25 +0200

    welcome Student

---
+++ back/student/src/main/java/com/Spring/Student/student/StudentApplication.java
@@ -0,0 +1,13 @@
+package com.Spring.Student.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+
+}
