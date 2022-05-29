3f4fa92f336d04c36dce54ac8ec51027ec504579
kimo <kimo84@gmail.com>
2022-05-29 11:10:40 +0200

    page login closes #5

--- front/Students/src/app/app.module.ts
+++ front/Students/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componnents/header/header.component';
+import { FooterComponent } from './componnents/footer/footer.component';
+import { LoginComponent } from './componnents/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
